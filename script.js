d88dd49c8656b5b212c1dd40749c871bba7c920f
AdrisPozosV <adrispozos@gmail.com>
2023-05-27 21:57:39 -0600

    Function Encriptar
    se realiza la funcion encriptar

--- script.js
+++ script.js
@@ -31,3 +31,16 @@ function btnEncriptar(){
 //*La letra "o" es convertida para "ober"
 //*La letra "u" es convertida para "ufat"
 
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+
+        }
+
+    }
+    return stringEncriptada;
+}
