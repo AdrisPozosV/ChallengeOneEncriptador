c0028a5db4b87fe343a646a71f7d164871328a18
AdrisPozosV <adrispozos@gmail.com>
2023-05-27 20:03:17 -0600

    Declaracion de Variables
    Se comienzan a declarar las variables a utilizar.

---
+++ script.js
@@ -0,0 +1,5 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+const copia = document.querySelector(".copiar");
+copia.style.display = "none";
+
