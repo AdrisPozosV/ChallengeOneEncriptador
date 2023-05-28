44769cda57584802b1dd3a48c05d03d3496d38e7
AdrisPozosV <adrispozos@gmail.com>
2023-05-27 22:43:16 -0600

    Update script

--- script.js
+++ script.js
@@ -1,55 +1,53 @@
 const textArea = document.querySelector(".text-area");
 const mensaje = document.querySelector(".mensaje");
 const copia = document.querySelector(".copiar");
-copia.style.display = "none";
+copia.style.display = "none"
 
 function validarTexto(){
     let textoEscrito = document.querySelector(".text-area").value;
-    let validador = textoEscrito.match(/^[a-z]*$/)
+    let validador = textoEscrito.match(/^[a-z]*$/);
 
-    if(validador || validador === 0){
-        alert("Solo son permitidas letras minisculas y sin acentos")
+    if(!validador || validador === 0) {
+        alert("Solo son permitidas letras minúsculas y sin acentos")
         location.reload();
         return true;
     }
 }
 
 function btnEncriptar(){
-    if(!validarTexto()){
-        const textoEncriptado = encriptar(textArea.value);
-        mensaje.value = textoEncriptado;
-        mensaje.style.backgroundImage = "none";
+    if(!validarTexto()) {
+        const textoEncriptado = encriptar(textArea.value)
+        mensaje.value = textoEncriptado
+        mensaje.style.backgroundImage = "none"
         textArea.value = "";
-        copia.style.display = block;
+        copia.style.display = "block"
+    
     }
 }
 
 //Llaves de encriptacion
-//*La letra "e" es convertida para "enter"
-//*La letra "i" es convertida para "imes"
-//*La letra "a" es convertida para "ai"
-//*La letra "o" es convertida para "ober"
-//*La letra "u" es convertida para "ufat"
+// `La letra "e" es convertida para "enter"`
+// `La letra "i" es convertida para "imes"`
+// `La letra "a" es convertida para "ai"`
+// `La letra "o" es convertida para "ober"`
+// `La letra "u" es convertida para "ufat"`
 
 function encriptar(stringEncriptada){
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringEncriptada = stringEncriptada.toLowerCase();
+    stringEncriptada = stringEncriptada.toLowerCase()
 
     for(let i = 0; i < matrizCodigo.length; i++){
         if(stringEncriptada.includes(matrizCodigo[i][0])){
             stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
-
         }
-
     }
-    return stringEncriptada;
+    return stringEncriptada
 }
 
 function btnDesencriptar(){
     const textoEncriptado = desencriptar(textArea.value)
     mensaje.value = textoEncriptado
     textArea.value = "";
-    
 }
 
 function desencriptar(stringDesencriptada){
